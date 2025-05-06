3cd8a533dc878508e6eb2cccab2a089d2face5b5
Nikesh Maharjan <nikeshmhr@gmail.com>
2025-05-06 11:52:29 +0400

    fix: message

--- packages/package-2/src/index.js
+++ packages/package-2/src/index.js
@@ -6,6 +6,6 @@ module.exports = package2;
 
 function package2() {
   const message = package1();
-  console.log(message);
+  console.log(`message from package 1 => ${message}`);
   return 'Hello from package2';
 }
