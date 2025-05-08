7931296e77b99f248d01e5cbb0549b40b3e980d6
Nikesh Maharjan <nikeshmhr@gmail.com>
2025-05-08 12:05:03 +0400

    feat: add test workflow (#6)
    * feat: add test workflow
    
    * fix: make test run
    
    * fix: packages publishes

--- packages/package-1/__tests__/package-1.test.js
+++ packages/package-1/__tests__/package-1.test.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const package1 = require('..');
+const package1 = require('../src');
 const assert = require('assert').strict;
 
-assert.strictEqual(package1(), 'Hello from package1');
+assert.strictEqual(package1(), 'Hello from package1 - Edit 100');
 console.info('package1 tests passed');
--- packages/package-2/__tests__/package-2.test.js
+++ packages/package-2/__tests__/package-2.test.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const package2 = require('..');
+const package2 = require('../src');
 const assert = require('assert').strict;
 
-assert.strictEqual(package2(), 'Hello from package2');
+assert.strictEqual(package2(), 'Hello from package2 - Edit 70');
 console.info('package2 tests passed');
