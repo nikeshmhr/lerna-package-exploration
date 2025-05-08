d974ae093bbc0f48ab2720ec002faa63aae50bdb
Nikesh Maharjan <nikeshmhr@gmail.com>
2025-05-08 11:10:59 +0400

    fix: packages publishes

--- packages/package-1/__tests__/package-1.test.js
+++ packages/package-1/__tests__/package-1.test.js
@@ -3,5 +3,5 @@
 const package1 = require('../src');
 const assert = require('assert').strict;
 
-assert.strictEqual(package1(), 'Hello from package1');
+assert.strictEqual(package1(), 'Hello from package1 - Edit 100');
 console.info('package1 tests passed');
--- packages/package-2/__tests__/package-2.test.js
+++ packages/package-2/__tests__/package-2.test.js
@@ -3,5 +3,5 @@
 const package2 = require('../src');
 const assert = require('assert').strict;
 
-assert.strictEqual(package2(), 'Hello from package2');
+assert.strictEqual(package2(), 'Hello from package2 - Edit 70');
 console.info('package2 tests passed');
