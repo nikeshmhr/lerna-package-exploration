f985caf4f6e41afb551d4d9db5b35316511962de
Nikesh Maharjan <nikeshmhr@gmail.com>
2025-06-16 09:54:39 +0545

    feat: export new APIs (#9)

--- packages/package-1/src/index.js
+++ packages/package-1/src/index.js
@@ -1,7 +1,13 @@
-'use strict';
+"use strict";
 
 module.exports = package1;
 
 function package1() {
-  return 'Hello from package1 - Edit 100';
+  return "Hello from package1 - Edit 100";
 }
+
+function package1a() {
+  return "Hello from package1a function";
+}
+
+module.exports.package1a = package1a;
--- packages/package-2/src/index.js
+++ packages/package-2/src/index.js
@@ -1,11 +1,17 @@
-'use strict';
+"use strict";
 
-const package1 = require('@mnikesh/service-sdk.package-1');
+const package1 = require("@mnikesh/service-sdk.package-1");
 
 module.exports = package2;
 
 function package2() {
   const message = package1();
   console.log(`message from package 1 => ${message}`);
-  return 'Hello from package2 - Edit 70';
+  return "Hello from package2 - Edit 70";
 }
+
+function package2a() {
+  return "Hello from package2a function";
+}
+
+module.exports.package2a = package2a;
